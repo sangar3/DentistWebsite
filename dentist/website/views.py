27244bdcea96ef876c4d6e98a476d004854f0201
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-08-02 12:16:31 -0500

    finihsed episode 6
    can showcase website progress on portfolio

--- dentist/website/views.py
+++ dentist/website/views.py
@@ -4,4 +4,16 @@ def home(request):
     return render(request,'home.html',{})
 
 def contact(request):
-    return render(request, 'contact.html', {})
+    if request.method == "POST": #IF STATEMENT IS CALLED ONCE USER FILLS OUT FORM
+        message_name = request.POST['message-name'] # NAME TAG
+        message_email = request.POST['message-email']# EMAIL TAG
+        message = request.POST['message']# MESSGAE TAG
+        return render(request, 'contact.html', {'message_name':message_name})
+
+    else:
+        return render(request, 'contact.html', {})
+
+
+
+
+
