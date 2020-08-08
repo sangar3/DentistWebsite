a477d6a27df3d21027cdcfe83cc2edf912d12255
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-08-08 12:58:38 -0500

    added appointment forms

--- dentist/website/views.py
+++ dentist/website/views.py
@@ -48,21 +48,24 @@ def appointment(request):
         your_schedule = request.POST['your-schedule']
         your_date = request.POST['your-date']
         your_message = request.POST['your-message']
-        #SENDING EMAIL
-        # send_mail(
-        #     subject,#subject
-        #     message, #message
-        #     message_email,#from email
-        #     ['sgarcia@mail.bradley.edu'], #to Email
-        # )
+        # SENDING EMAIL
+        appointment = "Name: " + your_name + "Phone:" + your_phone + "Email" + your_email + "Address: " + your_address + "Schedule: " + your_schedule + "Day: " + your_date + "Message: " + your_message
+        send_mail(
+            'Appointment Request',#subject
+            appointment, #message
+            your_email,#from email
+            ['sgarcia@mail.bradley.edu'], #to Email
+        )
 
         return render(request, 'appointment.html', {
             'your_name': your_name,
+            'your_phone': your_phone,
             'your_email':your_email,
             'your_address':your_address,
             'your_schedule': your_schedule,
-            'your_time':your_date,
+            'your_date':your_date,
             'your_message':your_message,
+
         })
 
     else:
